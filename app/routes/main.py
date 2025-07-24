ca7a7b12bb4af02d7a8f547ff6535c4e95eeee0b
d4lt <senadaniel08@gmail.com>
2025-07-24 11:47:33 -0300

    partial trilhas.html

--- app/routes/main.py
+++ app/routes/main.py
@@ -3,8 +3,14 @@ from ..models import Noticia
 
 main_routes = Blueprint("main", __name__)
 
+@main_routes.route("/home")
 @main_routes.route("/")
 def home():
-    
+    #get from api TODO
     
     return render_template("home.html")
+
+@main_routes.route("/trilhas")
+def trilhas():
+    
+    return render_template("trilhas.html")
